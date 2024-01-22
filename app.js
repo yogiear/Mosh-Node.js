c26b61afbc8306660ea60b08b30985a98d9ed19f
yogiear <yogieariana.ftmd18@gmail.com>
2024-01-22 09:48:11 +0700

    Creating and loading module in node.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// // // function sayHello(name) {
+// // //     console.log('Hello ' + name);
+// // // }
+
+// // // sayHello('Brother');
+
+// // var text = '';
+// // console.log(global.text);
+
+// console.log(module);
+
+// const logger = require('./logger');
+const log = require('./logger');
+
+// logger.log('message');
+log('message');
---
+++ logger.js
@@ -0,0 +1,11 @@
+var x =;
+var url = 'http//mylogger.io/log';
+
+function log(message) {
+    // Send an HTTP request
+    console.log(message)
+}
+
+// module.exports.log = log;
+module.exports = log;
+// // module.exports.endPoint = url;
