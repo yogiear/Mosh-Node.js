5d725fc5d75bf61a0e5cbf5b99721faec3ec6673
yogiear <yogieariana.ftmd18@gmail.com>
2024-01-22 11:02:13 +0700

    Trying path module.

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-// // // // function sayHello(name) {
-// // // //     console.log('Hello ' + name);
-// // // // }
+// // // // // function sayHello(name) {
+// // // // //     console.log('Hello ' + name);
+// // // // // }
 
-// // // // sayHello('Brother');
+// // // // // sayHello('Brother');
 
-// // // var text = '';
-// // // console.log(global.text);
+// // // // var text = '';
+// // // // console.log(global.text);
 
-// // console.log(module);
+// // // console.log(module);
 
-// // const logger = require('./logger');
-// const log = require('./logger');
+// // // const logger = require('./logger');
+// // const log = require('./logger');
 
-// // logger.log('message');
-// log('message');
+// // // logger.log('message');
+// // log('message');
 
-const path = require('path');
+// const path = require('path');
 
-var pathObj = path.parse(__filename);
+// var pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
+
+const os = require('node:os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log('Total Memory: ' + totalMemory);
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
