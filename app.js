b433e84f97a61f3f0cefd8f615e54d3a7be3e0e1
yogiear <yogieariana.ftmd18@gmail.com>
2024-01-22 20:48:48 +0700

    Trying events module with nodejs.org/docs syntax.

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
-// // // // // function sayHello(name) {
-// // // // //     console.log('Hello ' + name);
-// // // // // }
+// // // // // // // function sayHello(name) {
+// // // // // // //     console.log('Hello ' + name);
+// // // // // // // }
 
-// // // // // sayHello('Brother');
+// // // // // // // sayHello('Brother');
 
-// // // // var text = '';
-// // // // console.log(global.text);
+// // // // // // var text = '';
+// // // // // // console.log(global.text);
 
-// // // console.log(module);
+// // // // // console.log(module);
 
-// // // const logger = require('./logger');
-// // const log = require('./logger');
+// // // // // const logger = require('./logger');
+// // // // const log = require('./logger');
 
-// // // logger.log('message');
-// // log('message');
+// // // // // logger.log('message');
+// // // // log('message');
 
-// const path = require('path');
+// // // const path = require('path');
 
-// var pathObj = path.parse(__filename);
+// // // var pathObj = path.parse(__filename);
 
-// console.log(pathObj);
+// // // console.log(pathObj);
 
-const os = require('node:os');
+// // const os = require('node:os');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// // var totalMemory = os.totalmem();
+// // var freeMemory = os.freemem();
 
-console.log('Total Memory: ' + totalMemory);
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+// // console.log('Total Memory: ' + totalMemory);
+// // console.log(`Total Memory: ${totalMemory}`);
+// // console.log(`Free Memory: ${freeMemory}`);
+
+// const fs = require('node:fs');
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+// fs.readdir('../../Mosh Course/Mosh-GIT', function(err, files) {
+//     if (err) {
+//         console.log('Error', err);
+//     } else {
+//         console.log('Result', files);
+//     }
+// });
+
+const EventEmitter = require('node:events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+myEmitter.emit('event');
