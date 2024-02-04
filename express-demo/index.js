f0fb89b58dbfbd254b613388f600c15d610226c2
yogiear <yogieariana.ftmd18@gmail.com>
2024-02-04 18:15:41 +0700

    Post method with node and express js.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1'},
@@ -11,9 +14,40 @@ app.get('/', (req, res) => {
     res.send(courses);
 });
 
-
 app.get('/api/courses', (req, res) => {
     res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+    
+    if (result.error) {
+        res.status(400).send(result.error);
+        return;
+    }
+        
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course)
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // If not existing, return 404
+
+    // Validate
+    // If invalid, return 400 - Bad request
+
+    // Update course
+    // Return the updated course
 });
 
 app.get('/api/courses/:id', (req, res) => {
